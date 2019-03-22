00114f1e232df5b039154700705e52de9eefb5ff
Brandon <brandy@scarecrowarts.com>
2019-03-22 16:04:27 -0400

    lane logic start

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -18,5 +18,20 @@ import openfl.utils.Object;
 
 class PlayState extends FlxState
 {
-
+	private var playerLane:Int = 1;
+	
+	private function playerMovement():Void
+	{
+		switch (playerLane)
+		{
+			case 1:
+				//set player pos to lane 1
+			case 2:
+				//set player pos to lane 2
+			case 3:
+				//set player pos to lane 3
+			default:
+				//set player pos value to lane 1 value
+		}
+	}
 }
--- source/Player.hx
+++ source/Player.hx
@@ -8,5 +8,20 @@ import flixel.system.FlxAssets.FlxGraphicAsset;
 
 class Player extends FlxSprite 
 {
-	//just for the github thing i guess
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset)
+	{
+		super(X, Y, SimpleGraphic);
+		loadGraphic(AssetPaths.spr_player__png, true, 16, 16);
+
+		var frameRate:Int = 6;
+		animation.add("u", [0, 1, 2], frameRate);
+		drag.x = drag.y = 200;
+		
+		width = 8;
+		offset.x = 4;
+		height = 8;
+		offset.y = 6;
+
+		FlxG.log.add("added player");
+	}
 }
